55b5257658dc896c067cdde0496ea5ac6d836629
Bahar Shah <bahar@getdigraph.com>
2023-01-19 12:04:32 -0500

    precompile regexp

--- sanitize.go
+++ sanitize.go
@@ -7,39 +7,42 @@ import (
 
 const sanitize_replacement_string string = "REDACTED"
 
-var sensitiveFieldNames = []string{
-	`account[_|/-|\s](.[\w\s_/-]+)*key`,
-	`api[_|/-|\s](.[\w\s_/-]+)*key`,
-	`auth[_|/-|\s](.[\w\s_/-]+)*key`,
-	`azurerm-account-key`,
-	`client[_|/-](.[\w\s_/-]+)*key`,
-	`contrasena`,
-	`database[_|/-|\s](.[\w\s_/-]+)*key`,
-	`database[_|/-|\s](.[\w\s_/-]+)*pass`,
-	`database[_|/-|\s](.[\w\s_/-]+)*password`,
-	`db[_|/-|\s](.[\w\s_/-]+)*key`,
-	`db[_|/-|\s](.[\w\s_/-]+)*pass`,
-	`db[_|/-|\s](.[\w\s_/-]+)*password`,
-	`fetch-tfstate-headers`,
-	`key[_|/-|\s](.[\w\s_/-]+)*pass`,
-	`key[_|/-|\s](.[\w\s_/-]+)*password`,
-	`passwd`,
-	`password`,
-	`priv[_|/-|\s](.[\w\s_/-]+)*key`,
-	`private[_|/-|\s](.[\w\s_/-]+)*key`,
-	`pwd`,
-	`secret`,
-	`service[_|/-|\s](.[\w\s_/-]+)*key`,
-	`tfc-token`,
-	`username`,
+var preCompiledRegExpList = []*regexp.Regexp{
+	constructSensitiveRegExp(`account[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`api[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`auth[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`azurerm-account-key`),
+	constructSensitiveRegExp(`client[_|/-](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`contrasena`),
+	constructSensitiveRegExp(`database[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`database[_|/-|\s](.[\w\s_/-]+)*pass`),
+	constructSensitiveRegExp(`database[_|/-|\s](.[\w\s_/-]+)*password`),
+	constructSensitiveRegExp(`db[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`db[_|/-|\s](.[\w\s_/-]+)*pass`),
+	constructSensitiveRegExp(`db[_|/-|\s](.[\w\s_/-]+)*password`),
+	constructSensitiveRegExp(`fetch-tfstate-headers`),
+	constructSensitiveRegExp(`key[_|/-|\s](.[\w\s_/-]+)*pass`),
+	constructSensitiveRegExp(`key[_|/-|\s](.[\w\s_/-]+)*password`),
+	constructSensitiveRegExp(`passwd`),
+	constructSensitiveRegExp(`password`),
+	constructSensitiveRegExp(`priv[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`private[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`pwd`),
+	constructSensitiveRegExp(`secret`),
+	constructSensitiveRegExp(`service[_|/-|\s](.[\w\s_/-]+)*key`),
+	constructSensitiveRegExp(`tfc-token`),
+	constructSensitiveRegExp(`username`),
+}
+
+func constructSensitiveRegExp(filter string) *regexp.Regexp {
+	filterString := fmt.Sprintf(`(?:\"|\')(?P<key>(%s)+)(?:\"|\')(?:\:\s*)(?:\"|\')?(?P<value>[\w\s-\[\]]*)(?:\"|\')?`, filter)
+	return regexp.MustCompile(filterString)
 }
 
 func SanitizeValuesByKey(content []byte) ([]byte, error) {
-	for i := range sensitiveFieldNames {
-		filter := sensitiveFieldNames[i]
-		r := regexp.MustCompile(fmt.Sprintf(`(?:\"|\')(?P<key>(%s)+)(?:\"|\')(?:\:\s*)(?:\"|\')?(?P<value>[\w\s-\[\]]*)(?:\"|\')?`, filter))
+	for _, sensitiveRegExp := range preCompiledRegExpList {
 		template := fmt.Sprintf("\"$key\":\"%s\"", sanitize_replacement_string)
-		content = r.ReplaceAll(content, []byte(template))
+		content = sensitiveRegExp.ReplaceAll(content, []byte(template))
 	}
 	return content, nil
 }
